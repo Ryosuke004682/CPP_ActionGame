84ef89d30b8b2d1ef5536706349a2afe433324c6
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-10-06 14:49:09 +0900

    武器の当たり判定を作成

--- ActionGame/Source/ActionGame/Private/Items/Weapons/Weapon.cpp
+++ ActionGame/Source/ActionGame/Private/Items/Weapons/Weapon.cpp
@@ -5,6 +5,13 @@
 #include "Character/PlayerController_Core.h"
 #include "kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+
+AWeapon::AWeapon()
+{
+	WeaponBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Weapon Box"));
+	WeaponBox->SetupAttachment(GetRootComponent());
+}
 
 void AWeapon::Equip(USceneComponent* InParent, FName InSocketName)
 {
