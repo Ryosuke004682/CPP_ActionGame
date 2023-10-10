6f2d490b92e86b52c237a381a734c0958d95cf20
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2023-10-11 03:12:11 +0900

    当たった方向にノックバックさせる処理を実装

--- ActionGame/Source/ActionGame/Private/Enemy/Enemy.cpp
+++ ActionGame/Source/ActionGame/Private/Enemy/Enemy.cpp
@@ -3,6 +3,7 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "Components/CapsuleComponent.h"
 #include "ActionGame/DebugMacros.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 AEnemy::AEnemy()
 {
@@ -13,13 +14,22 @@ AEnemy::AEnemy()
 	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
 	GetMesh()->SetGenerateOverlapEvents(true);
 	GetCapsuleComponent()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera , ECollisionResponse::ECR_Ignore);
-	
-
 }
 
 void AEnemy::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+void AEnemy::PlayHitReactMontage(const FName& SectionName)
+{
+	UAnimInstance* AnimInstance = GetMesh()->GetAnimInstance();
+
+	if (AnimInstance && HitReactMontage)
+	{
+		AnimInstance->Montage_Play(HitReactMontage);
+		AnimInstance->Montage_JumpToSection(SectionName, HitReactMontage);
+	}
 }
 
 void AEnemy::Tick(float DeltaTime)
@@ -31,11 +41,61 @@ void AEnemy::Tick(float DeltaTime)
 void AEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
 void AEnemy::GetHit(const FVector& ImpactPoint)
 {
 	DRAW_SPHERE_COLOR(ImpactPoint , FColor::Orange);
+	PlayHitReactMontage(FName("FromLeft"));
+
+	DirectionalHitReact(ImpactPoint);
+
 }
 
+void AEnemy::DirectionalHitReact(const FVector& ImpactPoint)
+{
+	const FVector Forward = GetActorForwardVector();
+
+	//敵の位置 Z への低いインパクトポイント
+	const FVector ImpactLowered(ImpactPoint.X, ImpactPoint.Y, GetActorLocation().Z);
+	const FVector   ToHit = (ImpactLowered - GetActorLocation()).GetSafeNormal();
+
+	//Forward * ToHit = |Forward||ToHit| * cos(theta)
+	//|Forward| = 1 , |ToHit| = 1 , so Forward * ToHit = cos(theta)
+	const double CosTheta = FVector::DotProduct(Forward, ToHit);
+	//cos(θ)の逆余弦(アーク余弦)を取りθを得る
+	double Theta = FMath::Acos(CosTheta);
+	//ラジアンから度への変換
+	Theta = FMath::RadiansToDegrees(Theta);
+
+	// if CrossProct points down, Theta should be negative
+	const FVector CrossProduct = FVector::CrossProduct(Forward, ToHit);
+	if (CrossProduct.Z < 0)
+	{
+		Theta *= -1.f;
+	}
+	FName Section("FromBack");
+
+	if (Theta >= -45.f && Theta < 45.f)
+	{
+		Section = FName("FromFront");
+	}
+	else if (Theta >= -135.f && Theta < -45.f)
+	{
+		Section = FName("FromLeft");
+	}
+	else if (Theta >= -45.f && Theta < -135.f)
+	{
+		Section = FName("FromRight");
+	}
+	PlayHitReactMontage(Section);
+
+	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + CrossProduct * 100.f, 5.f, FColor::Blue, 5.f);
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(1, 5.f, FColor::Green, FString::Printf(TEXT("Theta: %f"), Theta));
+	}
+	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + Forward * 60.f, 5.f, FColor::Red, 5.f);
+	UKismetSystemLibrary::DrawDebugArrow(this, GetActorLocation(), GetActorLocation() + ToHit * 60.f, 5.f, FColor::Green, 5.f);
+}
